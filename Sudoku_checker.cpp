7d10af60a1450c4c1e408d4bd694c27d93514b88
quakquakduck <134900569+quakquakduck@users.noreply.github.com>
2023-07-21 10:45:48 +0800

    Update Sudoku_checker.cpp
    原題
    http://domen111.github.io/UVa-Easy-Viewer/?10957

--- Sudoku_checker.cpp
+++ Sudoku_checker.cpp
@@ -3,27 +3,30 @@ using namespace std;
 
 int case_i; // 第幾筆測資
 int matrix[14][14]; //盤面
-set<int> bset;
+
 int r[14][14], c[14][14], a[5][5][14]; 
+
 /*
-r[row][n]
-c[col][n]
-a[][][n]
+r[row][n] 每一列的n出現次數。
+c[col][n] 每一行的n出現次數。
+a[][][n] 每一個九宮格的n出現次數。
+每次操作後這些變數會被插入bset
+若出現2以上的數值變代表此作法無解。
 */
+set<int> bset; //判斷illegal
 
-int type;
-int ways;
+int type; 
+int ways;//計算方法數
 
-int st_r, st_c;
-
+// situation[type]
 string situation[5] = {"", "Illegal", "Unique", "Ambiguous", "Impossible"};
 
 
-bool not_used(int row, int col, int n){
+bool not_used(int row, int col, int n){ // 判斷數字是否被填過
     return !r[row][n] and !c[col][n] and !a[row/3][col/3][n];
 }
 
-void set_reset(){
+void set_reset(){ // 重製bset，並重新填入0與1
     bset.clear();
     bset.insert(1);
     bset.insert(0);
@@ -31,10 +34,13 @@ void set_reset(){
 
 void plus_fc(int row, int col, int n){ // 填入
     if (n > 0){
+        // 三個出現次數的變數都加1
         r[row][n]++;
         c[col][n]++;
         a[row/3][col/3][n]++;
         matrix[row][col] = n;
+
+        // 將次數記錄進bset
         bset.insert(r[row][n]);
         bset.insert(c[col][n]);
         bset.insert(a[row/3][col/3][n]);
@@ -43,11 +49,12 @@ void plus_fc(int row, int col, int n){ // 填入
 }
 
 void minus_fc(int row, int col, int n){ // 消除
-    
+    // 三個出現次數的變數都減1
     r[row][n]--;
     c[col][n]--;
     a[row/3][col/3][n]--;
     matrix[row][col] = 0;
+    // 重製
     set_reset();
     return;
 }
@@ -58,39 +65,39 @@ void dfs(int row, int col){
         return;
     }
     
-    if (row == 9){
+    if (row == 9){ // 最底層
         ways++;
-        if (ways >= 2){
+        if (ways >= 2){ // 檢測ambiquous
             type = 3;
         }
         return;
     }
 
-    if (!matrix[row][col]){
+    if (!matrix[row][col]){ // 判斷是否為空格
         for (int k = 1 ; k <= 9 ; k++){
              
-            if (type){
+            if (type){ // 檢測ambiguous
                 return;
             }
 
             if (not_used(row, col, k)){
                 
-                plus_fc(row, col, k);
+                plus_fc(row, col, k); // 出現次數增加
                 
-                if (col == 8){
+                if (col == 8){ // 進入下一列
                     dfs(row+1, 0);
                 }
-                else {
+                else { // 同一列，進入下一行
                     dfs(row, col+1);
                 }
 
-                minus_fc(row, col, k);
+                minus_fc(row, col, k); // 出現次數減少
                 
             }
         }
         
     }
-    else {
+    else { // 否則繼續往下搜
         if (col == 8){
             dfs(row+1, 0);
         }
@@ -107,7 +114,7 @@ int main(){
         
         case_i++;
 
-        // 初始化
+        // memset
         for (int j = 0 ; j < 14 ; j++){
             for (int i = 0 ; i < 14 ; i++){
                 r[j][i] = 0;
@@ -123,37 +130,33 @@ int main(){
         }
         type = 0;
         ways = 0;
-        st_r = -1;
-        st_c = -1;
         set_reset();
 
 
         // 輸入
         for (int j = 0 ; j < 9 ; j++){
             for (int i = 0 ; i < 9 ; i++){
-                /*if (st_r == -1 and matrix[j][i]){
-                    st_r = j;
-                    st_c = i;
-                }*/
                 cin >> matrix[j][i];
                 plus_fc(j, i, matrix[j][i]);
+
+                // 判斷是否為Illegal
                 if (bset.count(2)){
                     type = 1;
                 }
             }
         }
 
-
+        // 若非Illegal，則開始DFS
         if (!type){
             dfs(0, 0);
         }
 
         
-        if (!type){
-            if (ways){
+        if (!type){ // 方法數<2
+            if (ways){ // Unique
                 type = 2;
             }
-            else {
+            else { // Impossible
                 type = 4;
             }
         }
